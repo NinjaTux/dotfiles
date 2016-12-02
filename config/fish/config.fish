39adb170ede6e9806783c523f16939842faf6448
ninjatux <valerio.barrila@gmail.com>
2016-12-02 11:53:59 +0100

    added new brefile, new fish functions, aliases

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,11 +1,17 @@
 #!/bin/fish
 
+## navigation
+alias d "cd ~/Dropbox"
+alias dl "cd ~/Downloads"
+alias dk "cd ~/Desktop"
+alias w "cd ~/workspace"
+
 ## vim
 alias v "nvim"
 alias vim "nvim"
 alias nvim "env NVIM_TUI_ENABLE_TRUE_COLOR=1 nvim"
 alias vimrc "nvim ~/.config/nvim/init.vim"
-
+alias httpdump "sudo tcpdump -i en0 -n -s 0 -w - | grep -a -o -E \"Host\: .*|GET \/.*\""
 ## git
 alias g "git"
 alias ga "git add"
@@ -14,5 +20,7 @@ alias gm "git commit -m"
 alias gp "git push"
 
 ## misc shell
+alias h "history"
+alias j "jobs"
 alias l "ls -la"
 alias c "pygmentize -g"
