c6c4b3bbe64c30c991e60e2da172a520d6fa42a2
Valerio Barrila <valerio.barrila@gmail.com>
2020-04-13 08:56:46 +0200

    add iterm, htop, and vscode extensions

---
+++ code.extensions.zsh
@@ -0,0 +1,13 @@
+#!/usr/bin/env zsh
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension EditorConfig.EditorConfig
+code --install-extension imperez.smarty
+code --install-extension mauve.terraform
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode.Go
+code --install-extension octref.vetur
+code --install-extension redhat.vscode-yaml
+code --install-extension tomphilbin.gruvbox-themes
+code --install-extension vscode-icons-team.vscode-icons
