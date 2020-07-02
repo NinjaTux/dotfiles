ce954adf8d7b45f1cffa67616e5f65d68f6a42d6
Valerio <ninjatux@belzebu.local>
2020-07-02 11:37:43 +0200

    add folder to path

--- config/shell/exports.zsh
+++ config/shell/exports.zsh
@@ -18,7 +18,7 @@ export LC_CTYPE=en_US.UTF-8
 export LC_ALL=en_US.UTF-8
 
 # PATH
-export PATH=$HOME/bin:/usr/local/bin:$PATH
+export PATH=$HOME/bin:/usr/local/sbin:/usr/local/bin:$PATH
 
 # oh-my-zsh
 export ZSH=$HOME/.oh-my-zsh
